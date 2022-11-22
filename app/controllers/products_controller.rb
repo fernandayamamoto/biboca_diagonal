2919bece863c38fbd5def0118abc28952aa1428f
fernanda yamamoto fee.yamamoto@gmail.com <fee.yamamoto@gmail.com>
2022-11-22 16:30:21 -0300

    cloudinary first test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,6 +34,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :description)
+    params.require(:product).permit(:name, :price, :description, :photo)
   end
 end
