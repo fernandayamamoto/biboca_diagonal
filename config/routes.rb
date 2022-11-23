1054577579e2abe05fb4d7d92ffeeadc113b2b4c
Milena Neves Domingues <milena_neves95@hotmail.com>
2022-11-23 16:03:40 -0300

    order path ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :products do
     resources :orders, only: %i[create show]
   end
+  resources :orders, only: %i[index]
 end
